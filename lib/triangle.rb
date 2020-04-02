1051c53cc98cf53d66d2a36aca1ea1487f75adf8
Bikram Sodhi <biksodhi@gmail.com>
2020-04-02 13:40:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,8 +27,6 @@ class Triangle
   end
 
   class TriangleError < StandardError
-    def message
-      "The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. This is a principle known as the triangle inequality. Further, each side must be larger than 0"
-    end
+
   end
 end
