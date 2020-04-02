aa52cbb682d39c09c66bfdfd0293082d69e88aad
Bikram Sodhi <biksodhi@gmail.com>
2020-04-02 13:39:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -30,4 +30,5 @@ class Triangle
     def message
       "The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. This is a principle known as the triangle inequality. Further, each side must be larger than 0"
     end
+  end
 end
